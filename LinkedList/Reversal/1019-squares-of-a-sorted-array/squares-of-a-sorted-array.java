265d56d5c39f4d8b7532917c16460f90e6959770
pushpakkk-creates <pushpak.create619@gmail.com>
2026-06-17 01:49:47 +0530

    Time: 1 ms (100.00%) | Memory: 47 MB (93.78%) - LeetSync

---
+++ LinkedList/Reversal/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int [] result = new int[n];
+        
+        int i = 0;
+        int j = n-1;
+
+        int k = n-1;
+        while(k >= 0){
+            int a = nums[i] * nums[i];
+            int b = nums[j] * nums[j];
+
+            if(a > b){
+                result[k] = a;
+                i++;
+            }
+            else{
+                result[k] = b;
+                j--;
+            }
+            k--;
+        }
+        return result;
+    }
+}
